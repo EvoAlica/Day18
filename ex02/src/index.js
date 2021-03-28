a1479ae195800fdf173e4cfe01be76814581dc22
Tarik <79983026+EvoAlica@users.noreply.github.com>
2021-03-28 23:30:47 +0200

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,42 +1,35 @@
 // Only change code below this line
-
 function* myGenerator() {
-    yield* insideGenerator1();
-    yield* insideGenerator2();
-    yield* insideGenerator3();
+  yield* insideGenerator1();
+  yield* insideGenerator2();
+  yield* insideGenerator3();
 }
-function* insideGenerator1() {
-    for (var x = 1; x <= 5; x++) {
-        yield x;
-    }
-}
-function* insideGenerator2() {
-    for (var x = 10; x <= 15; x++) {
-        yield x;
-    }
 
-     
+function* insideGenerator1() {
+  for (var x = 1; x <= 5; x++) {
+    yield x;
+  }
 }
+
+function* insideGenerator2() {
+  for (var x = 10; x <= 15; x++) {
+    yield x;
+  }
+}
+
 function* insideGenerator3() {
-    for (var x = 6; x <= 9; x++) {
-        yield x;
-    }
+  for (var x = 6; x <= 9; x++) {
+    yield x;
+  }
 }
 
 let iterator = myGenerator();
 
 let fifteenArray = [];
-let fifteenArrayStr = "";
-for (let i = 0; i <= 15; i++) {
-    let { value } = iterator.next();
-    fifteenArray.push(value);
-    if (value) {
-        fifteenArrayStr += value + "#, ";
-    } else {
-        fifteenArrayStr += value + "!";
-    }
+for (let i = 0; i < 16; i++) {
+  let iter = iterator.next();
+  fifteenArray[i] = iter.value + (iter.done ? "!" : "#");
 }
-console.log(fifteenArray);
-console.log(fifteenArrayStr);
-// Only change code above this line 
+  console.log(fifteenArray.join(", "));
+// Only change code above this line
 module.exports = { fifteenArray, myGenerator };
